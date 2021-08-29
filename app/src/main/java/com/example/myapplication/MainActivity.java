c2ee70e957b636b0c754ec77bb5edc4befa0e7eb
ChenZw1111 <chenzhiwei@lespark.us>
2021-08-29 19:55:42 +0800

    merge

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image_view);
         String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
         System.out.println("dev_hotfix修改activity");
+        RequestOptions requestOptions = new RequestOptions();
         Glide
                 .with(this)
                 .load(url)
