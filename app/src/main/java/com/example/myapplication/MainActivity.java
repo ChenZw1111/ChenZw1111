423076427569bf6959a602bda1b1215a0e1eeb1f
ChenZw1111 <chenzhiwei@lespark.us>
2021-08-29 20:12:55 +0800

    seconde commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.image_view);
         String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
-        RequestOptions requestOptions = new RequestOptions();
         Glide
                 .with(this)
                 .load(url)
